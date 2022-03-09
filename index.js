e04a5950c79981374a9dc4b5d93398e46dbf6a0b
Kunal <krinsahuja.01@gmail.com>
2022-03-09 12:56:02 +0530

    connected mongoDB server and added models

---
+++ index.js
@@ -0,0 +1,43 @@
+const {ApolloServer}=require("apollo-server")
+const gql=require("graphql-tag")
+const mongoose=require('mongoose')
+
+const Post=require('./models/Post')
+const {MONGODB}=require('./config.js')
+
+const typeDefs=gql`
+type Post{
+    id:ID! 
+    body:String! 
+    username:String! 
+    createdAt: String!
+}
+    type Query{
+        getPosts:[Post]
+    }
+`
+const resolvers={
+    Query :{
+        async getPosts(){
+            try{
+                const posts=await Post.find({})
+                return posts
+            }catch(err){
+                throw new Error(err)
+            }
+        }
+    }
+}
+
+const server=new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(MONGODB,{useNewUrlParser:true})
+.then(()=>{
+    console.log("connected to Mongo DB")
+    return server.listen({port:3000});
+}).then((res)=>{
+     console.log(`server running at ${res.url}`);
+})
